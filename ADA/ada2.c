0061ef8ff5b397106ed2d98b12c1209981d3087f
imshridhar <shridharhavinal@gmail.com>
2025-03-29 18:46:40 +0530

    adding programs

---
+++ ADA/ada2.c
@@ -0,0 +1,65 @@
+// program 2
+// Design and implement C/C++ Program to find Minimum Cost Spanning Tree of a given
+// connected undirected graph using Prim's algorithm.
+#include<stdio.h>
+#define INF 999
+int prim(int c[10][10],int n,int s)
+{
+    int v[10],i,j,sum=0,ver[10],d[10],min,u;
+    for(i=1; i<=n; i++)
+    {
+        ver[i]=s;
+        d[i]=c[s][i];
+        v[i]=0;
+    }
+    v[s]=1;
+    for(i=1; i<=n-1; i++)
+    {
+        min=INF;
+        for(j=1; j<=n; j++)
+            if(v[j]==0 && d[j]<min)
+            {
+                min=d[j];
+                u=j;
+            }
+        v[u]=1;
+        sum=sum+d[u];
+        printf("\n%d -> %d sum=%d",ver[u],u,sum);
+        for(j=1; j<=n; j++)
+            if(v[j]==0 && c[u][j]<d[j])
+            {
+                d[j]=c[u][j];
+                ver[j]=u;
+            }
+    }
+    return sum;
+}
+void main()
+{
+    int c[10][10],i,j,res,s,n;
+    printf("\nEnter n value:");
+    scanf("%d",&n);
+    printf("\nEnter the graph data:\n");
+    for(i=1; i<=n; i++)
+        for(j=1; j<=n; j++)
+            scanf("%d",&c[i][j]);
+    printf("\nEnter the source node:");
+    scanf("%d",&s);
+    res=prim(c,n,s);
+    printf("\nCost=%d",res);
+    getchar();
+}
+
+// output
+// Enter n value:4
+// Enter the graph data:
+// 0 10 20 0
+// 10 0 30 5
+// 20 30 0 15
+// 0 5 15 0
+// Enter the souce node:1
+// 1 -> 1 sum=0
+// 1 -> 2 sum=10
+// 2 -> 4 sum=15
+// 2 -> 3 sum=45
+// Cost=45
